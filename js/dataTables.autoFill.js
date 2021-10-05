f5ae790b59a3cbe34ae51b44879882d4f5374f06
Allan Jardine <allan@datatables.net>
2021-10-05 12:49:42 +0000

    Package manager rebuild

--- js/dataTables.autoFill.js
+++ js/dataTables.autoFill.js
@@ -163,10 +163,39 @@ $.extend( AutoFill.prototype, {
 
 		this._focusListener();
 
-		this.dom.handle.on( 'mousedown', function (e) {
+		this.dom.handle.on( 'mousedown touchstart', function (e) {
 			that._mousedown( e );
 			return false;
 		} );
+
+		$(window).on('resize', function() {
+			var handle = $('div.dt-autofill-handle');
+			if(handle.length > 0 && that.dom.attachedTo !== undefined) {
+				that._attach(that.dom.attachedTo)
+			}
+		})
+
+		let orientationReset = function() {
+			that.s.handle = {
+				height: false,
+				width: false
+			};
+			$(that.dom.handle).css({
+				'height': '',
+				'width': ''
+			})
+			if(that.dom.attachedTo !== undefined) {
+				that._attach(that.dom.attachedTo)
+			}
+		}
+
+		$(window)
+			.on('orientationchange', function() {
+				setTimeout(function() {
+					orientationReset();
+					setTimeout(orientationReset, 150);
+				}, 50);
+			});
 
 		return this;
 	},
@@ -256,7 +285,7 @@ $.extend( AutoFill.prototype, {
 
 		// Might need to go through multiple offset parents
 		var offset = this._getPosition( node, this.dom.offsetParent );
-
+		
 		this.dom.attachedTo = node;
 		handle
 			.css( {
@@ -325,6 +354,10 @@ $.extend( AutoFill.prototype, {
 			} );
 
 			this.dom.background.appendTo( 'body' );
+			this.dom.background.one('click', () => {
+				this.dom.background.remove();
+				this.dom.list.remove();
+			})
 			this.dom.list.appendTo( 'body' );
 
 			this.dom.list.css( 'margin-top', this.dom.list.outerHeight()/2 * -1 );
@@ -376,7 +409,7 @@ $.extend( AutoFill.prototype, {
 		}
 
 		// if target is not in the columns available - do nothing
-		if ( dt.columns( this.c.columns ).indexes().indexOf( colIndx ) === -1 ) {
+		if ( dt.columns( this.c.columns ).indexes().indexOf( colIndx ) === -1 || end.row === -1) {
 			return;
 		}
 
@@ -550,16 +583,18 @@ $.extend( AutoFill.prototype, {
 		}
 		else {
 			$(dt.table().body())
-				.on( 'mouseenter'+namespace, 'td, th', function (e) {
+				.on( 'mouseenter'+namespace+' touchstart'+namespace, 'td, th', function (e) {
 					that._attach( this );
 				} )
-				.on( 'mouseleave'+namespace, function (e) {
+				.on( 'mouseleave'+namespace+'touchend'+namespace, function (e) {
 					if ( $(e.relatedTarget).hasClass('dt-autofill-handle') ) {
 						return;
 					}
 
 					that._detach();
 				} );
+
+			
 		}
 	},
 
@@ -640,10 +675,16 @@ $.extend( AutoFill.prototype, {
 		};
 
 		$(document.body)
-			.on( 'mousemove.autoFill', function (e) {
+			.on( 'mousemove.autoFill touchmove.autoFill', function (e) {
 				that._mousemove( e );
+				// If it is a touch event then when the touch ends we need to remove the handle
+				if(e.type === 'touchmove') {
+					$(document.body).one('touchend.autoFill', function() {
+						that._detach();
+					})
+				}
 			} )
-			.on( 'mouseup.autoFill', function (e) {
+			.on( 'mouseup.autoFill touchend.autoFill', function (e) {
 				that._mouseup( e );
 			} );
 
@@ -684,12 +725,13 @@ $.extend( AutoFill.prototype, {
 	{	
 		var that = this;
 		var dt = this.s.dt;
-		var name = e.target.nodeName.toLowerCase();
+		var target = !e.type.includes('touch') ? e.target : document.elementFromPoint(e.touches[0].clientX, e.touches[0].clientY);
+		var name = target.nodeName.toLowerCase();
 		if ( name !== 'td' && name !== 'th' ) {
 			return;
 		}
 
-		this._drawSelection( e.target, e );
+		this._drawSelection( target, e );
 		this._shiftScroll( e );
 	},
 
@@ -841,9 +883,13 @@ $.extend( AutoFill.prototype, {
 		var runInterval = false;
 		var scrollSpeed = 5;
 		var buffer = 65;
+
+		// Different values if using a touchscreen
+		var pageX = !e.type.includes('touch') ? e.pageX - window.scrollX :e.touches[0].clientX;
+		var pageY = !e.type.includes('touch') ? e.pageY - window.scrollY :e.touches[0].clientY;
 		var
-			windowY = e.pageY - document.body.scrollTop,
-			windowX = e.pageX - document.body.scrollLeft,
+			windowY = pageY,
+			windowX = pageX,
 			windowVert, windowHoriz,
 			dtVert, dtHoriz;
 
@@ -865,17 +911,17 @@ $.extend( AutoFill.prototype, {
 
 		// DataTables scrolling calculations - based on the table's position in
 		// the document and the mouse position on the page
-		if ( scroll.dtTop !== null && e.pageY < scroll.dtTop + buffer ) {
+		if ( scroll.dtTop !== null && pageY < scroll.dtTop + buffer ) {
 			dtVert = scrollSpeed * -1;
 		}
-		else if ( scroll.dtTop !== null && e.pageY > scroll.dtTop + scroll.dtHeight - buffer ) {
+		else if ( scroll.dtTop !== null && pageY > scroll.dtTop + scroll.dtHeight - buffer ) {
 			dtVert = scrollSpeed;
 		}
 
-		if ( scroll.dtLeft !== null && e.pageX < scroll.dtLeft + buffer ) {
+		if ( scroll.dtLeft !== null && pageX < scroll.dtLeft + buffer ) {
 			dtHoriz = scrollSpeed * -1;
 		}
-		else if ( scroll.dtLeft !== null && e.pageX > scroll.dtLeft + scroll.dtWidth - buffer ) {
+		else if ( scroll.dtLeft !== null && pageX > scroll.dtLeft + scroll.dtWidth - buffer ) {
 			dtHoriz = scrollSpeed;
 		}
 
@@ -906,12 +952,7 @@ $.extend( AutoFill.prototype, {
 			this.s.scrollInterval = setInterval( function () {
 				// Don't need to worry about setting scroll <0 or beyond the
 				// scroll bound as the browser will just reject that.
-				if ( scroll.windowVert ) {
-					document.body.scrollTop += scroll.windowVert;
-				}
-				if ( scroll.windowHoriz ) {
-					document.body.scrollLeft += scroll.windowHoriz;
-				}
+				window.scrollTo(window.scrollX + (scroll.windowHoriz ? scroll.windowHoriz : 0), window.scrollY + (scroll.windowVert ? scroll.windowVert : 0))
 
 				// DataTables scrolling
 				if ( scroll.dtVert || scroll.dtHoriz ) {
