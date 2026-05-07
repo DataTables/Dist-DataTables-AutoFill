394acdfb11a1522dc7307f8c2a83a86dee07d4ea
dtbuild <dtbuild@rigil>
2026-05-07 13:35:13 +0000

    f9651880d8006c384a7bcd7e894b1223edcf5615 Dev: Reformat file with prettier-m
    No functional change
    
    dc0a079b24c2c6e8c5cdfec55a627664df2715a3 New: Remove jQuery dependency from AutoFill
    New: Rewritten in Typescript
    
    Still a WIP - not quite complete:
    
    * Need to check the i18n API
    * Defaults / init typing isn't right
    * Build hasn't been tried (it won't work atm)
    * Styling files still to be done
    
    36c92421cedfa79221f36750a4bd18f6c79f98eb Dev: AutoFill rewritten in TS without jQuery
    
    53ed49f85cfe99763cd5cb74a4b028cb13724257 Dev: Rearranging code for TS integration with DataTables' interfaces
    
    3de7d8e6300368a7c356e430bf81fd9fcccfbe38 Build: Setting up the build for TS
    
    548e5ee81c27f82d859a71d4266954543633a318 Dev: Missed copying the integration file types
    
    97622a10b5900c5fa25f1604063b329d6360792f Dev fix: Correct context for API
    
    2cd383620cda364449f6ef78dcc2944b9f1ad5f0 New: Expanded CSS variables for styling AutoFill
    
    Updated: CSS prefix is `dtaf-` for consistency with the other DataTables extensions. If you had custom styling that used `dt-autofill-` class names, you will need to update the CSS rules.
    
    Updated: Positioning of the popover box when there is a choice of options is now done with flexbox
    
    4dc77701fff9f4763f5c83f75736890ed308273b New: Improved styling integration with the styling frameworks. Popover now looks like it is a modal from the host styling library.
    
    fbfa537e67b724c1dc8a0674c415fff07dce5799 Dev: Changing how the DT internal settings work for augmentation by the extensions
    
    Fix: Throw an error if `columns.editField` is given as an array
    
    6a9bee82b70a0db43d925108d9957710afee51f9 Dev fix: Remove throw for multiple field
    
    I got this part wrong in the port
    
    eb9a18dc2f965ae7897aad1e656776a3863b3de8 Dev: keytable is a property only defined if KeyTable is included
    
    979b0f32b826bec103c18bdbaf117fcb58160a51 Dev: Examples updated to load DataTables on index pages
    
    691fdcc577709e07879acd6d7768d301a8362e72 Examples: Remove specific mention of jQuery
    
    No longer relevent in this context
    
    a4228d0fe93e51cfecc3d9670f22323fba8b9a89 WIP: Update to use DataTables Api / Dom / util exports
    
    WIP: Update file headers to common DataTables styling
    
    WIP: Update build process to use the common `ts_extension` routine
    
    f27743c8c737617fac875be824100cbe8ba7076f Examples: Update to remove jQuery initialisation version of each example, and focus on the Vanilla JS code for simplicity and consistency throughout the DataTables examples.
    
    7b35b82ca1a4a26afcda8c47fa575b5391e89377 Docs: Updated for the more consistent naming convention used for DataTables' defined types in DT3
    
    3a4a92765a3e292d060778e80e99a90ec4e84554 Docs: Consistent naming for DataTables types
    
    e0a383299196c813cfa9f03b14030fdf9553d08d Release 3.0.0-beta.1
    
    4144fcbc4ab94ab80adad44669bfedd6ebd2162e Update: ES6 output target from TypeScript
    
    871a84416bbda2ca849851aab6086eb1fbdda3ae Dev: For the moment, need noImplicitThis - will be updated in future
    
    a5e749ee159621ca4f09306327262405aa4b77cc Dev: TS files shouldn't be executable
    
    Sync to source repo @a5e749ee159621ca4f09306327262405aa4b77cc

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,16 +1,186 @@
-// Type definitions for DataTables AutoFill
-//
-// Project: https://datatables.net/extensions/autofill/, https://datatables.net
-// Definitions by:
-//   SpryMedia
-//   Andy Ma <https://github.com/andy-maca>
-
-/// <reference types="jquery" />
-
-import DataTables, {Api} from 'datatables.net';
-
-export default DataTables;
+import DataTables__default, { ColumnSelector, Api, ApiCellMethods, CellIdxWithVisible, Dom, Context } from 'datatables.net';
 export * from 'datatables.net';
+export { default } from 'datatables.net';
+
+interface Classes {
+    btn: string;
+    close: string;
+    closeable: string;
+}
+interface Defaults {
+    /** Ask user what they want to do, even for a single option */
+    alwaysAsk: boolean;
+    /** Show a "Close" button in the list of options */
+    closeButton: boolean;
+    /** What will trigger a focus */
+    focus: 'click' | 'focus' | 'hover' | null;
+    /** Columns to provide auto fill for */
+    columns: ColumnSelector;
+    /** Enable AutoFill on load */
+    enable: boolean;
+    /** Update the cells after a drag */
+    update: boolean;
+    /** Editor instance for automatic submission */
+    editor: any;
+    /** Enable vertical fill */
+    vertical: boolean;
+    /** Enable horizontal fill */
+    horizontal: boolean;
+}
+interface Action {
+    available(dt: Api, cells: SelectedCells[][]): boolean;
+    option(dt: Api, cells: SelectedCells[][]): string;
+    execute(dt: Api, cells: SelectedCells[][], node: Dom | null): void | false;
+}
+interface SelectedCells {
+    cell: ApiCellMethods<any>;
+    data: any;
+    index: CellIdxWithVisible;
+    label: string;
+    set?: any;
+}
+
+/*! AutoFill for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
+ */
+
+declare class AutoFill {
+    /**
+     * AutoFill actions. The options here determine how AutoFill will fill the
+     * data in the table when the user has selected a range of cells. Please see
+     * the documentation on the DataTables site for full details on how to
+     * create plug- ins.
+     */
+    static actions: Record<string, Action>;
+    /** Class names used by AutoFill for customisation */
+    static classes: Classes;
+    /** Defaults */
+    static defaults: Defaults;
+    /** AutoFill version */
+    static version: string;
+    enabled(): boolean;
+    enable(flag?: boolean): this;
+    disable(): this;
+    private c;
+    private dom;
+    private s;
+    constructor(dt: Api | Context, opts?: Partial<Defaults>);
+    /**
+     * Initialise the RowReorder instance
+     */
+    private _init;
+    /**
+     * Display the AutoFill drag handle by appending it to a table cell. This is
+     * the opposite of the _detach method.
+     *
+     * @param node Cell to insert the handle into
+     */
+    private _attach;
+    /**
+     * Determine can the fill type should be. This can be automatic, or ask the
+     * end user.
+     *
+     * @param cells Information about the selected cells from the key up
+     *   function
+     */
+    private _actionSelector;
+    /**
+     * Remove the AutoFill handle from the document
+     */
+    private _detach;
+    /**
+     * Draw the selection outline by calculating the range between the start
+     * and end cells, then placing the highlighting elements to draw a rectangle
+     *
+     * @param target End cell
+     * @param e Originating event
+     * @private
+     */
+    private _drawSelection;
+    /**
+     * Use the Editor API to perform an update based on the new data for the
+     * cells
+     *
+     * @param cells Information about the selected cells from the key up
+     *   function
+     */
+    private _editor;
+    /**
+     * Emit an event on the DataTable for listeners
+     *
+     * @param name Event name
+     * @param args Event arguments
+     */
+    private _emitEvent;
+    /**
+     * Attach suitable listeners (based on the configuration) that will attach
+     * and detach the AutoFill handle in the document.
+     */
+    private _focusListener;
+    private _focusListenerRemove;
+    /**
+     * Get the position of a node, relative to another, including any scrolling
+     * offsets.
+     *
+     * @param node Node to get the position of
+     * @param targetHost Node to use as the parent
+     * @return Offset calculation
+     */
+    private _getPosition;
+    /**
+     * Start mouse drag - selects the start cell
+     *
+     * @param e Mouse down event
+     */
+    private _mousedown;
+    /**
+     * Mouse drag - selects the end cell and update the selection display for
+     * the end user
+     *
+     * @param e Mouse move event
+     */
+    private _mousemove;
+    /**
+     * End mouse drag - perform the update actions
+     *
+     * @param e Mouse up event
+     * @private
+     */
+    private _mouseup;
+    /**
+     * Create an array with a range of numbers defined by the start and end
+     * parameters passed in (inclusive!).
+     *
+     * @param start Start
+     * @param end End
+     */
+    private _range;
+    /**
+     * Move the window and DataTables scrolling during a drag to scroll new
+     * content into view. This is done by proximity to the edge of the scrolling
+     * container of the mouse - for example near the top edge of the window
+     * should scroll up. This is a little complicated as there are two elements
+     * that can be scrolled - the window and the DataTables scrolling view port
+     * (if scrollX and / or scrollY is enabled).
+     *
+     * @param e Mouse move event object
+     */
+    private _shiftScroll;
+    /**
+     * Update the DataTable after the user has selected what they want to do
+     *
+     * @param result Return from the `execute` method - can be false internally
+     *   to do nothing. This is not documented for plug-ins and is used only by
+     *   the cancel option.
+     * @param cells Information about the selected cells from the key up
+     *   function, augmented with the set values
+     */
+    private _update;
+}
+
+// Type definitions for DataTables AutoFill
+
+
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * DataTables' types integration
@@ -20,7 +190,7 @@ declare module 'datatables.net' {
 		/**
 		 * autoFill extension options
 		 */
-		autoFill?: boolean | ConfigAutoFill;
+		autoFill?: boolean | Partial<Defaults>;
 	}
 
 	interface ConfigLanguage {
@@ -33,7 +203,7 @@ declare module 'datatables.net' {
 	interface Api<T> {
 		/**
 		 * AutoFill methods container
-		 * 
+		 *
 		 * @returns Api for chaining with the additional autoFill methods
 		 */
 		autoFill: ApiAutoFill<T>;
@@ -43,73 +213,13 @@ declare module 'datatables.net' {
 		/**
 		 * AutoFill class
 		 */
-		AutoFill: {
-			/**
-			 * Create a new AutoFill instance for the target DataTable
-			 */
-			new (dt: Api<any>, settings: boolean | ConfigAutoFill): AutoFill;
-
-			/**
-			 * AutoFill version
-			 */
-			version: string;
-
-			/**
-			 * Default configuration values
-			 */
-			defaults: ConfigAutoFill;
-		}
+		AutoFill: AutoFill;
 	}
 }
-
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Options
  */
-
-interface ConfigAutoFill {
-	/**
-	 * Always ask the end user if an action should be taken or not
-	 */
-	alwaysAsk?: boolean;
-
-	/**
-	 * Select the columns that can be auto filled
-	 */
-	columns?: string | number[];
-
-	/**
-	 *
-	 * Attach an Editor instance for database updating
-	 */
-	editor?: any;
-
-	/**
-	 *
-	 * Initial enablement state of AutoFill
-	 */
-	enable?: boolean;
-
-	/**
-	 * Action that will cause the auto fill drag handle to appear in a cell
-	 */
-	focus?: 'click' | 'focus' | 'hover' | null;
-
-	/**
-	 * Enable / disable user ability to horizontally drag and fill
-	 */
-	horizontal?: boolean;
-
-	/**
-	 * Control automatic update of data when a fill drag is completed
-	 */
-	 update?: boolean;
-
-	/**
-	 * Enable / disable user ability to vertically drag and fill
-	 */
-	vertical?: boolean;
-}
 
 interface ConfigAutoFillLanguage {
 	/**
@@ -148,7 +258,6 @@ interface ConfigAutoFillLanguage {
 	info?: string;
 }
 
-
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * API
  */
@@ -160,14 +269,14 @@ interface ApiAutoFill<T> {
 interface ApiAutoFillMethods<T> extends Api<T> {
 	/**
 	 * Disable AutoFill. Please note that this disallows future interactions with the table (until re-enabled).
-	 * 
+	 *
 	 * @returns DataTables Api instance
 	 */
 	disable(): Api<T>;
 
 	/**
 	 * Enable end user and API modification of the focused cells in the DataTable. Differing levels of enablement are available via the optional parameter.
-	 * 
+	 *
 	 * @param flag can be true or false to signify whether to enable or disable
 	 * @returns DataTables Api instance
 	 */
@@ -175,7 +284,7 @@ interface ApiAutoFillMethods<T> extends Api<T> {
 
 	/**
 	 * This method will return a boolean value indicating if AutoFill is enabled or not on the selected table.
-	 * 
+	 *
 	 * @returns boolean signifying if autofill is enables
 	 */
 	enabled(): boolean;
